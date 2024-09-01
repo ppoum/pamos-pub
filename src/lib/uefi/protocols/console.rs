fd69c4cbfae303ac81e22b04f5d982d6ef2237c2
ppoum <pam@poum.ca>
2024-09-01 13:35:32 -0400

    Implement Write for SimpleTextOutputProtocol

--- src/lib/uefi/protocols/console.rs
+++ src/lib/uefi/protocols/console.rs
@@ -1,3 +1,5 @@
+use core::fmt::{self, Write};
+
 use crate::uefi::{status::Status, string::CStr16};
 
 pub type Output = SimpleTextOutputProtocol;
@@ -8,6 +10,47 @@ pub struct SimpleTextOutputProtocol(RawSimpleTextOutputProtocol);
 impl SimpleTextOutputProtocol {
     pub fn write(&mut self, s: &CStr16) -> Status {
         unsafe { (self.0.output_string)(&mut self.0, s.as_ptr()) }
+    }
+}
+
+impl Write for SimpleTextOutputProtocol {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        // Convert string from u8 bytes to u16 UCS-2
+        const BUF_SIZE: usize = 256;
+        let mut buffer = [0_u16; BUF_SIZE];
+        let mut utf16_buf = [0_u16; 2];
+        let mut i = 0;
+
+        for char in s.chars() {
+            if i == BUF_SIZE - 2 {
+                // Flush buffer
+                // Safety: Buffer only contains UCS-2 characters and always
+                //         ends with a null byte
+                let str = unsafe { CStr16::from_u16_unsafe(&buffer) };
+                self.write(str);
+                buffer = [0_u16; BUF_SIZE];
+                i = 0;
+            }
+
+            let bytes = char.encode_utf16(&mut utf16_buf);
+            let byte = if bytes.len() == 1 {
+                bytes[0]
+            } else {
+                // NOTE: This is a bit weird, we could be returning an error mid-print
+                // Might want to change the behaviour? Pre-parse the whole string? Too expensive?
+                return Err(fmt::Error);
+            };
+
+            buffer[i] = byte;
+            i += 1;
+        }
+
+        // Flush remaining data
+        // Safety: Buffer only contains UCS-2 characters and always
+        //         ends with a null byte
+        let str = unsafe { CStr16::from_u16_unsafe(&buffer) };
+        self.write(str);
+        Ok(())
     }
 }
 
